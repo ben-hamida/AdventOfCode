b3530046aa9a2f10180faea32e583b93fbedecc1
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-01 10:51:44 +0100

    First puzzle

---
+++ 2024/1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿var lines = File
+    .ReadLines("input.txt")
+    .Select(a => a.Split("   "))
+    .Select(x => (int.Parse(x[0]), int.Parse(x[1])))
+    .ToArray();
+
+var firstList = lines.Select(x => x.Item1).OrderBy(x => x);
+var secondList = lines.Select(x => x.Item2).OrderBy(x => x);
+
+var sum = firstList.Zip(secondList).Select(x => Math.Abs(x.First - x.Second)).Sum();
+
+Console.WriteLine(sum);
---
+++ 2024/1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿var lines = File
+    .ReadLines("input.txt")
+    .Select(x => x.Split("   "))
+    .Select(x => (long.Parse(x[0]), long.Parse(x[1])))
+    .ToArray();
+
+var firstList = lines.Select(x => x.Item1);
+var secondList = lines.Select(x => x.Item2);
+
+var sum = firstList
+    .Select(a => (a, secondList.Count(b => a == b)))
+    .Select(x => x.a * x.Item2)
+    .Sum();
+
+Console.WriteLine(sum);
