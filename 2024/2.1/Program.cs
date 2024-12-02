a3995475366cabc52f4b3494cc4f511921519a84
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-02 09:06:42 +0100

    Puzzle 2

---
+++ 2024/2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿var count = File
+    .ReadLines("input.txt")
+    .Select(line => line
+        .Split(' ')
+        .Select(int.Parse)
+        .ToArray())
+    .Count(IsSafe);
+
+Console.WriteLine(count);
+return;
+
+bool IsSafe(int[] report) => IsSafeInDirection(report, isDesc: false) || IsSafeInDirection(report, isDesc: true);
+
+bool IsSafeInDirection(int[] report, bool isDesc)
+{
+    for (var i = 0; i < report.Length - 1; i++)
+    {
+        var diff = isDesc ? report[i] - report[i + 1] : report[i + 1] - report[i];
+        if (diff is < 1 or > 3)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ 2024/2.2/Program.cs
@@ -0,0 +1,37 @@
+﻿var count = File
+    .ReadLines("input.txt")
+    .Select(line => line
+        .Split(' ')
+        .Select(int.Parse)
+        .ToArray())
+    .Count(IsSafe);
+
+
+Console.WriteLine(count);
+return;
+
+bool IsSafe(int[] report) =>
+    IsSafeInDirection(report, isDesc: true, hasRemovedUnsafeLevel: false) ||
+    IsSafeInDirection(report, isDesc: false, hasRemovedUnsafeLevel: false);
+
+bool IsSafeInDirection(int[] report, bool isDesc, bool hasRemovedUnsafeLevel)
+{
+    for (var i = 0; i < report.Length - 1; i++)
+    {
+        var diff = isDesc ? report[i] - report[i + 1] : report[i + 1] - report[i];
+        if (diff is >= 1 and <= 3)
+        {
+            continue;
+        }
+
+        if (hasRemovedUnsafeLevel)
+        {
+            return false;
+        }
+
+        return IsSafeInDirection([..report[..i], ..report[(i + 1)..]], isDesc, true) ||
+               IsSafeInDirection([..report[..(i + 1)], ..report[(i + 2)..]], isDesc, true);;
+    }
+
+    return true;
+}
