830c35ec8ef1fe6443566b17b6f7b2897df3ad61
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-03 07:06:08 +0100

    Puzzle day 3

---
+++ 2024/3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+var input = string.Join("", File.ReadLines("input.txt"));
+
+var sum = Regex
+    .Matches(input, @"mul\((\d\d?\d?),(\d\d?\d?)\)")
+    .Select(m => m.Groups)
+    .Aggregate(0, (i, group) => i + int.Parse(group[1].Value) * int.Parse(group[2].Value));
+
+Console.WriteLine(sum);
---
+++ 2024/3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+var input = string.Join("", File.ReadLines("input.txt"));
+
+var (_, sum) = Regex
+    .Matches(input, @"mul\((\d\d?\d?),(\d\d?\d?)\)|do\(\)|don't\(\)")
+    .Aggregate((Enabled: true, Sum: 0), (tuple, match) => match.Value switch
+    {
+        var value when value.StartsWith("mul") && tuple.Enabled =>
+            (tuple.Enabled, tuple.Sum + int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value)),
+        "do()" => (true, tuple.Sum),
+        "don't()" => (false, tuple.Sum),
+        _ => (tuple.Enabled, tuple.Sum)
+    });
+
+Console.WriteLine(sum);
