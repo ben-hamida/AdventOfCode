d749d4903428d94735d0eeabded2f4d504eb084e
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-04 09:38:23 +0100

    Puzzle day 4

---
+++ 2024/4.1/Program.cs
@@ -0,0 +1,103 @@
+﻿var lines = File.ReadLines("input.txt").ToArray();
+
+var count = 0;
+for (var i = 0; i < lines.Length; i++)
+{
+    for (var j = 0; j < lines[0].Length; j++)
+    {
+        if (lines[i][j] != 'X')
+        {
+            continue;
+        }
+
+        try
+        {
+            if (lines[i + 1][j] == 'M' && lines[i + 2][j] == 'A' && lines[i + 3][j] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i - 1][j] == 'M' && lines[i - 2][j] == 'A' && lines[i - 3][j] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i][j + 1] == 'M' && lines[i][j + 2] == 'A' && lines[i][j + 3] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i][j - 1] == 'M' && lines[i][j - 2] == 'A' && lines[i][j - 3] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i + 1][j + 1] == 'M' && lines[i + 2][j + 2] == 'A' && lines[i + 3][j + 3] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i - 1][j - 1] == 'M' && lines[i - 2][j - 2] == 'A' && lines[i - 3][j - 3] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i - 1][j + 1] == 'M' && lines[i - 2][j + 2] == 'A' && lines[i - 3][j + 3] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        try
+        {
+            if (lines[i + 1][j - 1] == 'M' && lines[i + 2][j - 2] == 'A' && lines[i + 3][j - 3] == 'S')
+            {
+                count++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+    }
+}
+
+Console.WriteLine(count);
---
+++ 2024/4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿var lines = File.ReadLines("input.txt").ToArray();
+
+var count = 0;
+for (var i = 1; i <  lines.Length -1; i++)
+{
+    for (var j = 1; j < lines[0].Length -1; j++)
+    {
+        if(lines[i][j] == 'A' &&
+           ((lines[i - 1][j - 1] == 'M' && lines[i + 1][j + 1] == 'S') ||
+           (lines[i - 1][j - 1] == 'S' && lines[i + 1][j + 1] == 'M')) &&
+           ((lines[i - 1][j + 1] == 'M' && lines[i + 1][j - 1] == 'S') ||
+            (lines[i - 1][j + 1] == 'S' && lines[i + 1][j - 1] == 'M')))
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
