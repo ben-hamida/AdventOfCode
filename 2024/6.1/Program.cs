6e4d55b6736e958a83a5bae15ebbaa55d63e818d
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-06 09:09:42 +0100

    Puzzle day 6

---
+++ 2024/6.1/Program.cs
@@ -0,0 +1,50 @@
+﻿var lines = File.ReadLines("input.txt").ToArray();
+
+var map = lines
+    .Index()
+    .SelectMany(x => x.Item
+        .Index()
+        .Select(y => (Content: y.Item, X: x.Index, Y: y.Index)))
+    .ToArray();
+
+var obstaclePositions = map
+    .Where(x => x.Content == '#')
+    .Select(x => (x.X, x.Y))
+    .ToHashSet();
+var guardPosition = map.First(x => x.Content == '^');
+var guard = (guardPosition.X, guardPosition.Y, Direction: Direction.Up);
+
+var visited = new HashSet<(int X, int Y)>();
+
+while (IsInsideMap(guard.X, guard.Y))
+{
+    visited.Add((guard.X, guard.Y));
+    var (x, y, _) = guard;
+    guard = guard.Direction switch
+    {
+        Direction.Up when HasObstacle(x - 1, y) => guard with { Direction = Direction.Right },
+        Direction.Down when HasObstacle(x + 1, y) => guard with { Direction = Direction.Left },
+        Direction.Left when HasObstacle(x, y - 1) => guard with { Direction = Direction.Up },
+        Direction.Right when HasObstacle(x, y + 1) => guard with { Direction = Direction.Down },
+        Direction.Up => guard with { X = guard.X - 1 },
+        Direction.Down => guard with { X = guard.X + 1 },
+        Direction.Left => guard with { Y = guard.Y - 1 },
+        Direction.Right => guard with { Y = guard.Y + 1 },
+        _ => throw new InvalidOperationException()
+    };
+}
+
+Console.WriteLine(visited.Count);
+return;
+
+bool IsInsideMap(int x, int y) => x >= 0 && x < lines.Length && y >= 0 && y < lines[0].Length;
+
+bool HasObstacle(int x, int y) => obstaclePositions.Contains((x, y));
+
+public enum Direction
+{
+    Up,
+    Right,
+    Down,
+    Left
+}
---
+++ 2024/6.2/Program.cs
@@ -0,0 +1,64 @@
+﻿var lines = File.ReadLines("input.txt").ToArray();
+
+var map = lines
+    .Index()
+    .SelectMany(x => x.Item
+        .Index()
+        .Select(y => (Content: y.Item, X: x.Index, Y: y.Index)))
+    .ToArray();
+
+var initialObstacles = map
+    .Where(position => position.Content == '#')
+    .Select(position => (position.X, position.Y))
+    .ToHashSet();
+
+var guardPosition = map.First(x => x.Content == '^');
+
+var count = map
+    .Where(position => position.Content == '.')
+    .Count(position => IsLoop(
+        (guardPosition.X, guardPosition.Y, Direction: Direction.Up),
+        [..initialObstacles, (position.X, position.Y)]));
+
+Console.WriteLine(count);
+return;
+
+bool IsLoop((int X, int Y, Direction Direction) initialState, HashSet<(int X, int Y)> obstacles)
+{
+    var previousStates = new HashSet<(int X, int Y, Direction Direction)>();
+
+    while (IsInsideMap(initialState.X, initialState.Y))
+    {
+        if (!previousStates.Add(initialState))
+        {
+            return true;
+        }
+
+        var (x, y, _) = initialState;
+        initialState = initialState.Direction switch
+        {
+            Direction.Up when obstacles.Contains((x - 1, y)) => initialState with { Direction = Direction.Right },
+            Direction.Down when obstacles.Contains((x + 1, y)) => initialState with { Direction = Direction.Left },
+            Direction.Left when obstacles.Contains((x, y - 1)) => initialState with { Direction = Direction.Up },
+            Direction.Right when obstacles.Contains((x, y + 1)) => initialState with { Direction = Direction.Down },
+            Direction.Up => initialState with { X = initialState.X - 1 },
+            Direction.Down => initialState with { X = initialState.X + 1 },
+            Direction.Left => initialState with { Y = initialState.Y - 1 },
+            Direction.Right => initialState with { Y = initialState.Y + 1 },
+            _ => throw new InvalidOperationException()
+        };
+    }
+
+    return false;
+}
+
+bool IsInsideMap(int x, int y) => x >= 0 && x < lines.Length && y >= 0 && y < lines[0].Length;
+
+
+public enum Direction
+{
+    Up,
+    Right,
+    Down,
+    Left
+}
