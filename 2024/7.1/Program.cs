d8a5e13288b192389c8f1972f8bb7036b6d6fe02
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-07 09:52:52 +0100

    Day 7

---
+++ 2024/7.1/Program.cs
@@ -0,0 +1,18 @@
+﻿var sum = File.ReadLines("input.txt")
+    .Select(line => line.Split(':'))
+    .Select(splitResult => new
+    {
+        testValue = long.Parse(splitResult[0]),
+        numbers = splitResult[1].Trim().Split(' ').Select(long.Parse).ToArray()
+    })
+    .Select(line => new
+    {
+        line.testValue,
+        possibleResults = line.numbers.Skip(1).Aggregate<long, IEnumerable<long>>(
+            [line.numbers[0]],
+            (current, number) => current.SelectMany(x => new[] { x + number, x * number }))
+    })
+    .Where(t => t.possibleResults.Any(result => result == t.testValue))
+    .Sum(t => t.testValue);
+
+Console.WriteLine(sum);
