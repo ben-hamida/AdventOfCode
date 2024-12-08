ea959caa416c427ed08eebdf4d2bf4579be98449
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-08 15:26:08 +0100

    Day 8

---
+++ 2024/8.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Immutable;
+
+var count = File.ReadLines("input.txt")
+    .Index()
+    .SelectMany(x => x.Item
+        .Index()
+        .Select(y => (Content: y.Item, X: x.Index, Y: y.Index)))
+    .Where(item => item.Content != '.')
+    .GroupBy(position => position.Content, position => (position.X, position.Y))
+    .Select(positions => positions
+        .SelectMany((first, i) => positions
+            .Skip(i + 1)
+            .SelectMany(second => new []
+            {
+                (first.X + (second.X - first.X) * 2, first.Y + (second.Y - first.Y) * 2),
+                (second.X + (first.X - second.X) * 2, second.Y + (first.Y - second.Y) * 2)
+            })))
+    .Aggregate(ImmutableHashSet.Create<(int X, int Y)>(), (acc, antinodes) => acc.Union(antinodes))
+    .Count(c => c is { X: >= 0 and < 50, Y: >= 0 and < 50 });
+
+Console.WriteLine(count);
---
+++ 2024/8.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+
+var count = File.ReadLines("input.txt")
+    .Index()
+    .SelectMany(x => x.Item.Index().Select(y => (Content: y.Item, X: x.Index, Y: y.Index)))
+    .Where(item => item.Content != '.')
+    .GroupBy(position => position.Content, position => (position.X, position.Y))
+    .Select(positions => positions
+        .SelectMany((a, i) => positions
+            .Skip(i + 1)
+            .SelectMany<(int X, int Y), (int X, int Y)>(b => [
+                ..Enumerable
+                    .Range(0, int.MaxValue)
+                    .Select<int, (int X, int Y)>(multiplier => (a.X + (b.X - a.X) * multiplier, a.Y + (b.Y - a.Y) * multiplier))
+                    .TakeWhile(result => IsInsideMap(result.X, result.Y)),
+                ..Enumerable
+                    .Range(0, int.MaxValue)
+                    .Select<int, (int X, int Y)>(multiplier => (b.X + (a.X - b.X) * multiplier, b.Y + (a.Y - b.Y) * multiplier))
+                    .TakeWhile(result => IsInsideMap(result.X, result.Y))
+            ])))
+    .Aggregate(ImmutableHashSet.Create<(int X, int Y)>(), (acc, antinodes) => acc.Union(antinodes))
+    .Count;
+
+Console.WriteLine(count);
+return;
+
+bool IsInsideMap(int x, int y) => x is >= 0 and < 50 && y is >= 0 and < 50;
