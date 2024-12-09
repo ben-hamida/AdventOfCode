2ba578b0b0f93e2472f7c25b2cc265281a14af26
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-09 10:08:45 +0100

    Day 9

---
+++ 2024/9.1/Program.cs
@@ -0,0 +1,33 @@
+﻿var disk = File.ReadAllText("input.txt")
+    .Select(c => int.Parse(c.ToString()))
+    .Select<int, Memory>((number, index) => index % 2 == 0
+        ? new FileMemory(number, index / 2)
+        : new FreeMemory(number))
+    .SelectMany(memory => Enumerable.Repeat<int?>(memory is FileMemory fileMemory ? fileMemory.Id : null, memory.Size))
+    .ToArray();
+
+var fileMemoryCount = disk.Count(m => m is not null);
+
+var compactedMemory = disk.ToList();
+foreach (var id in disk.Reverse().Index())
+{
+    if (id.Item is not null)
+    {
+        var emptyMemoryIndex = compactedMemory.IndexOf(null);
+        if(emptyMemoryIndex >= fileMemoryCount - 1)
+        {
+            break;
+        }
+
+        compactedMemory[emptyMemoryIndex] = id.Item;
+        compactedMemory[compactedMemory.Count - id.Index - 1] = null;
+    }
+}
+
+var checksum = compactedMemory.OfType<int>().Index().Sum(x => x.Index * (double)x.Item);
+
+Console.WriteLine(checksum);
+
+internal record Memory(int Size);
+internal record FreeMemory(int Size) : Memory(Size);
+internal record FileMemory(int Size, int Id) : Memory(Size);
---
+++ 2024/9.2/Program.cs
@@ -0,0 +1,41 @@
+﻿var disk = File.ReadAllText("input.txt")
+    .Select(c => int.Parse(c.ToString()))
+    .Select<int, DiskMemory>((number, index) => index % 2 == 0
+        ? new FileMemory(number, index / 2)
+        : new FreeMemory(number))
+    .ToArray();
+
+var compactedMemory = disk.ToList();
+foreach (var file in disk.OfType<FileMemory>().Reverse())
+{
+    foreach (var freeMemory in compactedMemory.TakeWhile(m => m != file).OfType<FreeMemory>())
+    {
+        if (freeMemory.Size >= file.Size)
+        {
+            var freeMemoryIndex = compactedMemory.IndexOf(freeMemory);
+            compactedMemory[compactedMemory.IndexOf(file)] = new FreeMemory(file.Size);
+            compactedMemory[freeMemoryIndex] = new FreeMemory(freeMemory.Size - file.Size);
+            compactedMemory.Insert(freeMemoryIndex, file);
+            break;
+        }
+    }
+}
+
+var checksum = compactedMemory
+    .SelectMany(m => Enumerable.Repeat((m as FileMemory)?.Id ?? 0, m.Size))
+    .Index()
+    .Sum(x => x.Index * (double)x.Item);
+
+Console.WriteLine(checksum);
+
+internal class DiskMemory(int size)
+{
+    public int Size { get; } = size;
+}
+
+internal class FreeMemory(int size) : DiskMemory(size);
+
+internal class FileMemory(int size, int id) : DiskMemory(size)
+{
+    public int Id => id;
+}
