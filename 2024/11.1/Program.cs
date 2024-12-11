a5566c2017e763b65654084f0a9a575e2e82eb7d
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-11 10:36:02 +0100

    d

---
+++ 2024/11.1/Program.cs
@@ -0,0 +1,26 @@
+﻿List<long> stones = [8069, 87014, 98, 809367, 525, 0, 9494914, 5];
+
+for (var i = 0; i < 25; i++)
+{
+    for (var j = 0; j < stones.Count; j++)
+    {
+        if (stones[j] == 0)
+        {
+            stones[j] = 1;
+            continue;
+        }
+
+        var numberString = stones[j].ToString();
+        if (numberString.Length % 2 == 0)
+        {
+            stones.Insert(j, long.Parse(numberString[..(numberString.Length / 2)]));
+            stones[j + 1] = long.Parse(numberString[(numberString.Length / 2)..]);
+            j++;
+            continue;
+        }
+
+        stones[j] *= 2024;
+    }
+}
+
+Console.WriteLine(stones.Count);
---
+++ 2024/11.2/Program.cs
@@ -0,0 +1,20 @@
+﻿var count = Enumerable
+    .Range(0, 75)
+    .Aggregate(
+        new long[] { 8069, 87014, 98, 809367, 525, 0, 9494914, 5 }.Select(number => (Number: number, Count: 1L)),
+        (current, _) => current
+            .SelectMany<(long Number, long Count), (long Number, long Count)>(stone => stone switch
+            {
+                (0, var count) => [(1L, count)],
+                var (number, count) when number.ToString().Length % 2 == 0 =>
+                [
+                    (long.Parse(number.ToString()[..(number.ToString().Length / 2)]), count),
+                    (long.Parse(number.ToString()[(number.ToString().Length / 2)..]), count)
+                ],
+                var (number, count) => [(number * 2024, count)]
+            })
+            .GroupBy(stone => stone.Number)
+            .Select(grouping => (Number: grouping.Key, Count: grouping.Sum(stone => stone.Count))))
+    .Sum(stone => stone.Count);
+
+Console.WriteLine(count); // 218817038947400
