49e163383c648b247fc967a292cfeaaaf451de6f
Änis Ben Hamida <anis.benhamida@se.com>
2024-12-19 17:54:09 +0100

    Day 19

---
+++ 2024/19.1/Program.cs
@@ -0,0 +1,32 @@
+﻿var lines = File.ReadLines("input.txt").ToArray();
+var towelPatterns = lines[0].Split(", ").ToHashSet();
+var designs = lines.Skip(2).ToArray();
+
+var possibleDesignsCount = designs.Count(design => IsPossible(design, towelPatterns));
+
+Console.WriteLine(possibleDesignsCount);
+return;
+
+static bool IsPossible(string design, HashSet<string> towelPatterns)
+{
+    var queue = new PriorityQueue<string, int>([(design, design.Length)]);
+    var checkedDesigns = new HashSet<string>();
+    while (queue.TryDequeue(out var remainingDesign, out _))
+    {
+        if (remainingDesign.Length == 0)
+        {
+            return true;
+        }
+
+        if (!checkedDesigns.Add(remainingDesign))
+        {
+            continue;
+        }
+
+        queue.EnqueueRange(towelPatterns
+            .Where(pattern => remainingDesign.StartsWith(pattern))
+            .Select(pattern => (remainingDesign[pattern.Length..], remainingDesign.Length - pattern.Length)));
+    }
+
+    return false;
+}
---
+++ 2024/19.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+var lines = File.ReadLines("input.txt").ToArray();
+var towelPatterns = lines[0].Split(", ").ToHashSet();
+var designs = lines.Skip(2).ToArray();
+
+var numberOfArrangements = designs.Sum(design => GetNumberOfArrangements("", design, towelPatterns, []));
+
+Console.WriteLine(numberOfArrangements);
+return;
+
+static long GetNumberOfArrangements(
+    string arrangement,
+    string targetDesign,
+    HashSet<string> towelPatterns,
+    ConcurrentDictionary<string, long> cache) =>
+    arrangement switch
+    {
+        _ when arrangement == targetDesign => 1,
+        _ when arrangement.Length >= targetDesign.Length => 0,
+        _ => cache.GetOrAdd(
+            arrangement,
+            arrangement => towelPatterns
+                .Select(pattern => arrangement + pattern)
+                .Where(targetDesign.StartsWith)
+                .Sum(nextArrangement => GetNumberOfArrangements(nextArrangement, targetDesign, towelPatterns, cache)))
+    };
